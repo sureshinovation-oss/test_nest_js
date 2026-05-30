215682e6217f9fb318da6ea92f4552c87e30b578
suresh <suresh.beliveme@gmail.com>
2026-05-30 13:32:09 +0530

    first commit

---
+++ src/hello/hello.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('hello')
+export class HelloController {
+  @Get()
+  getHello() {
+    return {
+      message: 'Hello from NestJS',
+      status: 'success',
+    };
+  }
+}
+
